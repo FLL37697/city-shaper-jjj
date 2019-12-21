31a1b92b85cd246fb8e79d848a99f3e32bf5d726
radioactiveBrainstorm <55724365+radioactiveBrainstorm@users.noreply.github.com>
2019-12-21 11:48:52 -0500

    Updates.

--- main.ts
+++ main.ts
@@ -3,23 +3,6 @@ let currentTime = 0
 let motoraAdjustment = 0
 let loopStart = 0
 let lastArmPos = 0
-function mission12Jonas1() {
-    motors.resetAll()
-    motors.largeBC.steer(0, 33, 2, MoveUnit.Rotations)
-    motors.stopAll()
-    pause(1000)
-    motors.largeBC.steer(0, -39, 2, MoveUnit.Rotations)
-}
-function mission12Jonas2() {
-    motors.largeBC.steer(0, 33, 2.6, MoveUnit.Rotations)
-    motors.largeBC.steer(0, 29, 0, MoveUnit.Rotations)
-    motors.stopAll()
-    pause(1000)
-    motors.largeBC.steer(0, -39, 3.5, MoveUnit.Rotations)
-}
-radioactiveBrainstorm.addMenuItem("mission12 Jonas1", function () {
-    mission12Jonas1()
-})
 function mission12() {
     motors.resetAll()
     motors.largeBC.steer(0, 25, 3, MoveUnit.Rotations)
@@ -27,15 +10,6 @@ function mission12() {
     motors.largeBC.steer(0, -25, 3, MoveUnit.Rotations)
     motors.stopAll()
 }
-radioactiveBrainstorm.addMenuItem("mission12 Jonas2", function () {
-    mission12Jonas2()
-})
-radioactiveBrainstorm.addMenuItem("Crane Mission", function () {
-    crane()
-})
-radioactiveBrainstorm.addMenuItem("mission12", function () {
-    mission12()
-})
 function crane() {
     motors.resetAll()
     radioactiveBrainstorm.moveUntilStall(motors.mediumA, radioactiveBrainstorm.Direction.UP, 30, 40, 100, false)
@@ -55,6 +29,51 @@ motors.largeBC.steer(0, 25, 2.5, MoveUnit.Rotations)
 pause(1000)
     radioactiveBrainstorm.moveUntilStall(motors.mediumA, radioactiveBrainstorm.Direction.DOWN, 20, 10, 100, true)
 motors.largeBC.steer(0, -50, 2, MoveUnit.Rotations)
+}
+radioactiveBrainstorm.addMenuItem("mission12", function () {
+    mission12()
+})
+radioactiveBrainstorm.addMenuItem("Crane Mission", function () {
+    crane()
+})
+function mission12Jonas1() {
+    motors.resetAll()
+    motors.largeBC.steer(0, 33, 2, MoveUnit.Rotations)
+    motors.stopAll()
+    pause(1000)
+    motors.largeBC.steer(0, -39, 2, MoveUnit.Rotations)
+}
+radioactiveBrainstorm.addMenuItem("mission12 blackcircle", function () {
+    mission12Jonas1()
+})
+radioactiveBrainstorm.addMenuItem("mission12 redcircle", function () {
+    mission12Jonas2()
+})
+function mission12Jonas2() {
+    motors.largeBC.steer(0, 33, 2.6, MoveUnit.Rotations)
+    motors.largeBC.steer(0, 29, 0, MoveUnit.Rotations)
+    motors.stopAll()
+    pause(1000)
+    motors.largeBC.steer(0, -39, 40, MoveUnit.Rotations)
+}
+radioactiveBrainstorm.addMenuItem("mission12 tancircle", function () {
+    mission12Jonas3()
+    mission12Jonas2()
+})
+radioactiveBrainstorm.addMenuItem("mission6 traffic", function () {
+    mission12Jonas3()
+    mission6trafficjam()
+})
+function mission12Jonas3() {
+    motors.largeBC.steer(0, 33, 2.6, MoveUnit.Rotations)
+    motors.largeBC.steer(0, 29, 0, MoveUnit.Rotations)
+}
+function mission6trafficjam() {
+    motors.largeBC.steer(0, 33, 2.6, MoveUnit.Rotations)
+    motors.largeBC.tank(-50, 50, 1, MoveUnit.Rotations)
+    motors.stopAll()
+    pause(1000)
+    motors.largeBC.steer(0, 39, 1, MoveUnit.Rotations)
 }
 lastArmPos = 0
 loopStart = 0
